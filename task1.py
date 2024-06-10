2717fb55172be3ba05aec3657ecd50ff14b80f26
Androsov Sergei <Androsov-07@mail.ru>
2024-06-10 11:57:58 +0300

    correction

--- task1.py
+++ task1.py
@@ -3,7 +3,6 @@ import random
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
-print(lst)
 data = pd.DataFrame({'whoAmI':lst})
 
 one_hot_data = pd.DataFrame(data=data, columns=['human', 'robot'])
@@ -11,4 +10,4 @@ one_hot_data.loc[data['whoAmI'] == 'human', 'human'] = True
 one_hot_data.loc[data['whoAmI'] != 'human', 'human'] = False
 one_hot_data.loc[data['whoAmI'] == 'robot', 'robot'] = True
 one_hot_data.loc[data['whoAmI'] != 'robot', 'robot'] = False
-print(one_hot_data)
+one_hot_data
